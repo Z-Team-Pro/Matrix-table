9e21d2dfd84a4153384afd49d959931766b5260b
Ramy Hakam <pencilsoft1@gmail.com>
2018-08-04 16:25:39 +0200

    reformat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,41 +8,42 @@ import { DataService } from './data.service';
 })
 export class AppComponent {
   title = 'app';
- prices:any;
+  prices:any;
   all=[];
+  head=[];
+  test=[];
   constructor(private data:DataService){
- this.prices=this.data.getdata();
-
-
-
-//console.log('prices',Object.keys(this.prices) );
-let keys=Object.keys(this.prices);
-let matrix=[];
-
-
-let header=['sa','su','mo','tu','we','th','fr'];
-let rows=['sa','su','mo','tu','we','th','fr'];
- 
-header.forEach(col => {
+   this.prices=this.data.getdata();
+   let keys=Object.keys(this.prices);
+   
+    keys.forEach(element =>{
+      var combine=element.split(' ');
+      if(this.head.indexOf(combine[0])==-1){
+         this.head.push(combine[0]);}
+         if(this.test.indexOf(combine[1])==-1){
+         this.test.push(combine[1]);}
+        
+    });
+    console.log('head',this.head);
+    console.log('test',this.test);
+   let header= this.head;
+   let rows= this.test;
+rows.forEach(col => {
   let l=Array();
-  rows.forEach( row=> {
-    let item= col +" "+row;
-   // console.log('this is item',item);
-   //console.log('this is item value',this.prices[item]);
-     l.push(this.prices[item]);
-     console.log('l',l);
+  header.forEach( row=> {
+    let item= row +" "+col;
 
+     l.push(this.prices[item]);
+   
   });
   let final_row= {'col':col,data:l};
   this.all.push(final_row);
 });
 
-console.log(this.all);
 
 
 
 
-//creatre 2 dimentions array from this data 
 
  
   }
